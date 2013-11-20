4512bb5208a20eb18899109ef3b7a4c196f3b953
Rich Edwards <richedw@gmail.com>
2013-11-20 15:32:32 -0600

    Add a failing test for .vegetarian withinPizza::Topping's initialize method.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -10,9 +10,15 @@ end
 describe Pizza::Topping do
 	describe '.initialize' do
  		it "sets the name of the topping" do
- 			topping = Pizza::Topping.new('olives')
+ 			topping = Pizza::Topping.new('olives') 		
 
  			expect(topping.name).to eq('olives')
+ 		end
+
+ 		it 'sets whether or not the topping is vegetarian' do
+ 			topping = Pizza::Topping.new 'bell peppers', vegetarian: true
+
+ 			expect(topping.vegetarian).to eq(true)
  		end
  	end
 
